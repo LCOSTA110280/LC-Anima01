506c2d381ad499ac7c50714dfb2e6a2264bebc97
LCOSTA110280 <LEOCOSRA110280@gMAIL.COM>
2023-02-22 17:41:12 -0300

    Animação !!!

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { Text, View, StyleSheet, TextInput, Button, Image, TouchableOpacity, Alert,Animated } from 'react-native';
 
-export default function App() {
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      larAnimada:new Animated.Value(150),
+      altAnimada:new Animated.Value(50),
+
+    }
+    Animated.timing(
+
+      this.state.altAnimada,
+      {
+        toValue:100,
+        duration:2000,
+        
+      }
+
+    ).start();
+    Animated.timing(
+
+      this.state.larAnimada,
+      {
+        toValue:300,
+        duration:1000,
+        
+      }
+
+    ).start();
+
+  }
+
+
+
+  render(){  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Animated.View style={{
+            height:this.state.altAnimada,
+            width:this.state.larAnimada,
+            backgroundColor:'blue',
+            alignItems:'center',
+            justifyContent:'center',
+
+      }}>
+        <Text style={styles.textoCarregando}>Carregando....</Text>
+      </Animated.View>
+
     </View>
   );
 }
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1,
+    paddingTop:20,
+    alignItems:'center',
+    justifyContent:'center'
   },
-});
+  viewanima:{
+
+  },
+  textoCarregando:{
+    fontSize:22,
+    color:'white',
+  }
+
+  
+})
+
+export default App;
+
